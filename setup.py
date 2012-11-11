66487694c24b238263371b922b89ce98eb4f4ab7
Krzysztof Dorosz <cypreess@gmail.com>
2012-11-12 00:35:22 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='django-mogilefs-storage',
-    version='0.1',
-    packages=['mogilefs_backend'],
+    version='0.2',
+    packages=['mogilefs_storage'],
     url='https://github.com/cypreess/django-mogilefs-storage',
     license='MIT',
     author='Krzysztof Dorosz',
